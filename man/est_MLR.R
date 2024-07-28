cefe8aea53cd5156dccc3becc62d6550e307d6ab
lylewk <lylewk@users.noreply.github.com>
2024-07-28 10:08:42 -0500

    Add files via upload

---
+++ man/est_MLR.R
@@ -0,0 +1,17 @@
+#' Estimate age and prediction interval using multiple linear regression
+#'
+#' @param area The desired area for prediction interval
+#' default = 0.95 for 95% prediction interval
+#'
+est.MLR<-function(area=0.95)
+{
+est=as.numeric(x.new%*%parms$Bx.y)
+hf=as.numeric(x.new%*%parms$inv.XX%*%x.new)
+se.pred=sqrt(parms$MSE*(1+hf))
+t.value=-qt((1-area)/2,170)
+lower=est-t.value*se.pred
+upper=est+t.value*se.pred
+sto=c(lower,est,upper)
+names(sto)=c('lwr','fit','upr')
+return(sto)
+}
